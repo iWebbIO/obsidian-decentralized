c3f967462cd9d568d5f7832bf168d644b5390ff5
Webb <82875181+iWebbIO@users.noreply.github.com>
2026-01-01 21:06:55 +0330

    Update main.ts

--- main.ts
+++ main.ts
@@ -258,7 +258,6 @@ class DesktopLANDiscovery implements ILANDiscovery {
             });
         };
 
-        sendBeacon();
         this.broadcastInterval = window.setInterval(sendBeacon, 2000);
     }
 
